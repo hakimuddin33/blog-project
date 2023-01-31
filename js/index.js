f0293c5ddd6cf4d650f25264245e6608b762f4ab
Hakim Uddin <hakimmh5707768@gmail.com>
2023-02-01 00:59:00 +0600

    blog project

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var menuIcon = document.getElementById('menu-icon');
+var menu = document.getElementById('menu');
+
+menuIcon.addEventListener('click',()=>{
+   if(menu.className === 'hidden'){
+    menu.classList.remove('hidden')
+   }else{
+    menu.classList.add('hidden')
+   }
+})
